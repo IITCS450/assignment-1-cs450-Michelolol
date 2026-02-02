c26a962a6ae8cce4f082e99d4b935c5f6d81f042
Michelolol <mvtan555@gmail.com>
2026-02-01 21:47:45 -0600

    procinfo started

--- src/procinfo.c
+++ src/procinfo.c
@@ -5,7 +5,16 @@
 static void usage(const char *a){fprintf(stderr,"Usage: %s <pid>\n",a); exit(1);}
 static int isnum(const char*s){for(;*s;s++) if(!isdigit(*s)) return 0; return 1;}
 int main(int c,char**v){
- if(c!=2||!isnum(v[1])) usage(v[0]);
- printf("TODO: implement procinfo\n");
+    if(c!=2||!isnum(v[1])) usage(v[0]);
+ 
+    char *pid = v[1];
+    char path[256];
+    char line[256];
+
+    
+ 
+ 
+ 
+    printf("TODO: implement procinfo\n");
  return 0;
 }
